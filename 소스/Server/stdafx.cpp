86eddec1a9b9a71ae9f730366a5d39a8bccbcebc
LaivY <dnsjdbstlr@naver.com>
2022-04-13 15:28:59 +0900

    서버에서 사용할 함수 추가
    [+] GetBoundingBox 함수 추가
    └ 몬스터 정보를 주면 해당 정보를 이용하여 바운딩박스를 반환한다.

--- 소스/Server/stdafx.cpp
+++ 소스/Server/stdafx.cpp
@@ -12,3 +12,34 @@ void errorDisplay(const int errNum, const char* msg)
 	//while (true);
 	LocalFree(lpMsgBuf);
 }
+
+namespace Utility
+{
+	using namespace DirectX;
+	BoundingOrientedBox GetBoundingBox(const MonsterData& monsterData)
+	{
+		XMMATRIX worldMatrix{ XMMatrixIdentity() };
+		
+		// look벡터는 +z축을 yaw만큼 회전시킨 벡터임
+		XMVECTOR look{ XMVectorSet(0.0f, 0.0f, 1.0f, 1.0f) };
+		XMMATRIX rotateMatrix{ XMMatrixRotationRollPitchYaw(0.0f, monsterData.yaw, 0.0f) };
+		look = XMVector3Transform(look, rotateMatrix);
+
+		// up벡터는 고정
+		XMVECTOR up{ XMVectorSet(0.0f, 1.0f, 0.0f, 1.0f) };
+
+		// right 벡터는 up벡터와 look벡터의 외적
+		XMVECTOR right{ XMVector3Cross(up, look) };
+
+		// 월드변환행렬은 1열이 right, 2열이 up, 3열이 look, 4열이 위치. 모두 정규화 되어있어야함.
+		worldMatrix.r[0] = XMVector3Normalize(right);
+		worldMatrix.r[1] = up;
+		worldMatrix.r[2] = XMVector3Normalize(look);
+		worldMatrix.r[3] = XMVectorSet(monsterData.pos.x, monsterData.pos.y, monsterData.pos.z, 1.0f);
+
+		// 중심이 (0, 0, 0)이고 대각선이 (1, 1, 1)인 바운딩박스라고 가정함
+		BoundingOrientedBox bb{ XMFLOAT3{ 0.0f, 0.0f, 0.0f }, XMFLOAT3{ 1.0f, 1.0f, 1.0f }, XMFLOAT4{ 0.0f, 0.0f, 0.0f, 1.0f } };
+		bb.Transform(bb, worldMatrix);
+		return bb;
+	}
+}
--- 소스/Server/stdafx.h
+++ 소스/Server/stdafx.h
@@ -2,6 +2,7 @@
 
 #include <WS2tcpip.h>
 #include <MSWSock.h>
+#include <DirectXCollision.h>
 #include <DirectXMath.h>
 #include <array>
 #include <chrono>
@@ -10,7 +11,14 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include "protocol.h"
 #pragma comment (lib, "WS2_32.LIB")
 #pragma comment (lib, "MSWSock.LIB")
 
 void errorDisplay(const int errNum, const char* msg);
+
+namespace Utility
+{
+	using namespace DirectX;
+	BoundingOrientedBox GetBoundingBox(const MonsterData& monsterData);
+}
