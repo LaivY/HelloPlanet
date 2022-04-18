f75d89edab6a2ffbf7e239da30331c5a9266f54c
LaivY <dnsjdbstlr@naver.com>
2022-04-18 15:09:14 +0900

    그림자맵에 렌더링을 위한 기하셰이더
    현재 그림자맵에 렌더링할 때마다 드로우콜하기 때문에 성능에 좋지않다.
    따라서 기하셰이더를 이용해서 한 번의 드로우콜로 4개의 그림자맵에 렌더링할 수 있도록 해야한다.

--- 소스/Client/Shader/shadow.hlsl
+++ 소스/Client/Shader/shadow.hlsl
@@ -123,3 +123,42 @@ PS_INPUT VS_ANIMATION_ALL(VS_INPUT input)
     output.positionH = mul(mul(output.positionW, g_shadowLight.lightViewMatrix[3]), g_shadowLight.lightProjMatrix[3]);
     return output;
 }
+
+struct GS_INPUT
+{
+    float4 positionW : SV_POSITION;
+};
+
+struct GS_OUTPUT
+{
+    float4 positionH : SV_POSITION;
+    uint RTIndex : SV_RenderTargetArrayIndex;
+};
+
+GS_INPUT VS_ANIMATION(VS_INPUT input)
+{
+    GS_INPUT output;
+    float4 posL = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    for (int i = 0; i < 4; ++i)
+    {
+        posL += input.boneWeight[i] * mul(input.position, g_boneTransformMatrix[input.boneIndex[i]]);
+    }
+    output.positionW = mul(mul(posL, g_meshTransformMatrix), g_worldMatrix);
+    return output;
+}
+
+[maxvertexcount(3 * SHADOWMAP_COUNT)]
+void GS(triangle GS_INPUT input[3], inout TriangleStream<GS_OUTPUT> output)
+{
+    for (int i = 0; i < SHADOWMAP_COUNT; ++i)
+    {
+        GS_OUTPUT result;
+        result.RTIndex = i;
+        for (int j = 0; j < 3; ++j)
+        {
+            result.positionH = mul(mul(input[j].positionW, g_shadowLight.lightViewMatrix[i]), g_shadowLight.lightProjMatrix[i]);
+            output.Append(result);
+        }
+        output.RestartStrip();
+    }
+}
